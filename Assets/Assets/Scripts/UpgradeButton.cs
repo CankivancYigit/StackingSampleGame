ac4f81dcfc3d46a840e7def2a2c0bcdfb1535a6d
CankivancYigit <cankivancyigit@gmail.com>
2022-04-04 22:17:10 +0300

    WIP : Upgrade button

--- Assets/Assets/Scripts/UpgradeButton.cs
+++ Assets/Assets/Scripts/UpgradeButton.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeButton : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI upgradeCostText;
+    
+    private static int _upgradeCost = 30;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        upgradeCostText.text = _upgradeCost.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (ScoreSystem.Instance.CoinCount >_upgradeCost)
+        {
+            gameObject.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void UpgradeButtonClicked()
+    {
+        _upgradeCost = (int)Mathf.Pow(_upgradeCost, 1.05f);
+        upgradeCostText.text = _upgradeCost.ToString();
     }
 }
